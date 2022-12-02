4e23beb0ad88a0e5460a0f6d444e7f44a0fcf626
cesarC <jefedeproduccion2019@gmail.com>
2022-12-02 00:14:45 -0400

    SUPERADO

--- 03.js
+++ 03.js
@@ -5,17 +5,22 @@
   No comentar la funcion 
 */
 function buscarAmigo(amigos, nombre) {
-  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
-  // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
-  // recibe un string llamado 'nombre'.
-  // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
+  // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También recibe un string llamado 'nombre'.
+
+  //Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
+
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
+  var nombres = amigos.find(x=> x.nombre == nombre)
+  return nombres;
+  //return amigos.find(i=>i.nombre===nombre);
+  
   
 }
+
 
 // No modifiques nada debajo de esta linea //
 
